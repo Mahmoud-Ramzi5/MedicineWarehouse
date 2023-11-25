30683c9338e2ccd6a81f280807a3284b1c3e40e7
mohamed majzoub <mohamed13majzoub@gmail.com>
2023-11-25 18:45:02 +0300

    latest frontend 3

---
+++ FrontEnd/lib/views/phone/medicine_details_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MedicineDetailsView extends StatefulWidget {
+  const MedicineDetailsView({super.key});
+
+  @override
+  State<MedicineDetailsView> createState() => _MedicineDetailsViewState();
+}
+
+class _MedicineDetailsViewState extends State<MedicineDetailsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Center(child: CircularProgressIndicator())],
+      ),
+    );
+  }
+}
