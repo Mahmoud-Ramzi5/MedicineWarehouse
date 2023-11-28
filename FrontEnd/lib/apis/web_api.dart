9ab5aa2fa7a3f91e119001f2d722d16b8714d45d
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2023-11-28 13:50:49 +0300

    Edited FrontEnd

--- FrontEnd/lib/apis/web_api.dart
+++ FrontEnd/lib/apis/web_api.dart
@@ -1,18 +1,21 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class WebApi {
-  static final uri = Uri.parse('http://10.0.2.2:8000/api/register');
+  static final loginUri = Uri.parse('');
+  static final addMedicineUri =
+      Uri.parse('http://127.0.0.1:8000/api/admin/new_medicine');
 
   WebApi();
 
   Future<bool> login(String username, String password) async {
     var response = await http.post(
-      uri,
+      loginUri,
       encoding: const Utf8Codec(),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
-        'Accept': "/",
+        'Accept': "application/json",
         'connection': 'keep-alive',
         'Accept-Encoding': 'gzip, deflate, br',
       },
@@ -28,5 +31,46 @@ class WebApi {
     } else {
       return false;
     }
+  }
+
+  Future<dynamic> addMedicine(
+      String expiryDate,
+      int quantityAvailable,
+      int price,
+      String enCommercialName,
+      String arCommercialName,
+      String enScientificName,
+      String arScientificName,
+      String enManufactureCompany,
+      String arManufactureCompany,
+      List<int> categoryIds,
+      Uint8List image) async {
+    var response = await http.post(
+      addMedicineUri,
+      encoding: const Utf8Codec(),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept': "application/json",
+        'connection': 'keep-alive',
+        'Accept-Encoding': 'gzip, deflate, br',
+      },
+      body: jsonEncode(
+        {
+          "expiry_date": expiryDate,
+          "quantity_available": quantityAvailable,
+          "price": price,
+          "en_commercial_name": enCommercialName,
+          "ar_commercial_name": arCommercialName,
+          "en_scientific_name": enScientificName,
+          "ar_scientific_name": arScientificName,
+          "en_manufacture_company": enManufactureCompany,
+          "ar_manufacture_company": arManufactureCompany,
+          "category_id": categoryIds,
+          //"image": image
+        },
+      ),
+    );
+    print(image.toList());
+    print(response.body);
   }
 }
--- FrontEnd/lib/views/web/web_main_view.dart
+++ FrontEnd/lib/views/web/web_main_view.dart
@@ -4,6 +4,7 @@ import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:test1/apis/web_api.dart';
 
 class WebMainView extends StatefulWidget {
   const WebMainView({super.key});
@@ -82,7 +83,20 @@ class _WebMainViewState extends State<WebMainView> {
                               fit: BoxFit.fill,
                             )),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  WebApi().addMedicine(
+                      "5/9/2027",
+                      1,
+                      1,
+                      "enCommercialName",
+                      "arCommercialName",
+                      "enScientificName",
+                      "arScientificName",
+                      "enManufactureCompany",
+                      "arManufactureCompany",
+                      [1, 18],
+                      webImage);
+                },
                 child: const Text(
                   'Upload',
                   style: TextStyle(fontWeight: FontWeight.bold),
