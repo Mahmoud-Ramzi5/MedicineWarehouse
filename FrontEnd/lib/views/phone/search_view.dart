a44ebc6036894d19b0bc071824dada2ca50f0147
mohamed majzoub <mohamed13majzoub@gmail.com>
2023-12-23 09:55:59 +0300

    working search in mobile

--- FrontEnd/lib/views/phone/search_view.dart
+++ FrontEnd/lib/views/phone/search_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test1/apis/phone_api.dart';
+import 'package:test1/classes/medicine.dart';
 
 class SearchView extends StatefulWidget {
   const SearchView({super.key});
@@ -10,6 +11,7 @@ class SearchView extends StatefulWidget {
 
 class _SearchViewState extends State<SearchView> {
   late final TextEditingController _search;
+  List<Medicine> _medicines = [];
   @override
   void initState() {
     _search = TextEditingController();
@@ -22,102 +24,109 @@ class _SearchViewState extends State<SearchView> {
     super.dispose();
   }
 
+  Future<void> _onSearchSubmitted(String value) async {
+    final medicines = await Api().search(value);
+    setState(() {
+      _medicines = medicines;
+    });
+  }
+
+  Widget _buildSearchResults() {
+    return ListView.builder(
+      physics: const PageScrollPhysics(),
+      padding: const EdgeInsets.all(10),
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        final medicine = _medicines[index];
+        return Card(
+          elevation: 5,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                height: 100,
+                width: 100,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: Colors.green,
+                ),
+                child: Image.network(
+                  'http://10.0.2.2:8000/storage/${medicine.imagePath}',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '${medicine.medicineTranslations["en"]["commercial_name"]}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Price: ${medicine.price}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Quantity: ${medicine.quantityAvailable}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+      itemCount: _medicines.length,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(5),
-        child: SearchBar(
-          controller: _search,
-          hintText: 'Search',
-          leading: const Icon(Icons.search),
-          shape: const MaterialStatePropertyAll(
-            BeveledRectangleBorder(),
-          ),
-          textStyle: const MaterialStatePropertyAll(
-            TextStyle(
-              fontSize: 20,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: SearchBar(
+              controller: _search,
+              hintText: 'Search',
+              leading: const Icon(Icons.search),
+              shape: const MaterialStatePropertyAll(
+                BeveledRectangleBorder(),
+              ),
+              textStyle: const MaterialStatePropertyAll(
+                TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              side: const MaterialStatePropertyAll(
+                BorderSide(
+                  color: Colors.green,
+                ),
+              ),
+              onSubmitted: _onSearchSubmitted,
             ),
           ),
-          side: const MaterialStatePropertyAll(
-            BorderSide(
-              color: Colors.green,
-            ),
+          Expanded(
+            child: _buildSearchResults(),
           ),
-          onSubmitted: (value) {
-            FutureBuilder(
-              future: Api().search(_search.text.toString()),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else {
-                  final medicines = snapshot.data;
-                  return ListView.builder(
-                    physics: const PageScrollPhysics(),
-                    padding: const EdgeInsets.all(10),
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        elevation: 5,
-                        child: Row(
-                          children: [
-                            Container(
-                                margin: const EdgeInsets.all(10),
-                                height: 100,
-                                width: 100,
-                                decoration: const ShapeDecoration(
-                                    shape: ContinuousRectangleBorder(),
-                                    color: Colors.green),
-                                child: Image.network(
-                                    'http://10.0.2.2:8000/storage/${medicines[index].imagePath}',
-                                    fit: BoxFit.cover)),
-                            Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Column(
-                                    children: [
-                                      Text(
-                                        '${medicines[index].medicineTranslations["en"]["commercial_name"]}',
-                                        style: const TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.all(20),
-                                        child: Text(
-                                          'Price: ${medicines[index].price}',
-                                          style: const TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ),
-                                      Text(
-                                        'Quantity: ${medicines[index].quantityAvailable}',
-                                        style: const TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                    itemCount: medicines!.length,
-                  );
-                }
-              },
-            );
-          },
-        ),
+        ],
       ),
     );
   }
